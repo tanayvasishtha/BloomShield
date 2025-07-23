48b72a026ac0a014df6396f157092378c3b6b2e1
Tanay Vasishtha <tanayvasishtha@gmail.com>
2025-07-23 17:14:36 +0530

    Updated app.py for YOLOv9 predictions with bounding boxes

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+import sys
+sys.path.append('yolov9')
+from models.common import DetectMultiBackend
+from utils.general import non_max_suppression, scale_coords
+from utils.torch_utils import select_device
 from flask import Flask, request, jsonify, render_template
 import torch
-from torch.utils.data import DataLoader
-from torchvision import transforms
 from PIL import Image
+import io
+import cv2
+import numpy as np
 import sqlite3
 import os
 
@@ -10,13 +16,10 @@ from train import SimpleCNN, class_to_idx  # Import model and class mapping
 
 app = Flask(__name__)
 
-# Load model
-num_classes = len(class_to_idx)
-model = SimpleCNN(num_classes)
-model.load_state_dict(torch.load('bloomshield_model.pth'))
+device = select_device('cpu')
+model = DetectMultiBackend('bloomshield_yolo_model.pt', device=device)
+model = model.model
 model.eval()
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
 
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -47,13 +50,23 @@ def predict():
         return jsonify({'error': 'No file part'})
     file = request.files['file']
     img = Image.open(file.stream).convert('RGB')
-    img = transform(img).unsqueeze(0).to(device)
-    with torch.no_grad():
-        output = model(img)
-        pred = torch.argmax(output, 1).item()
-    class_name = list(class_to_idx.keys())[list(class_to_idx.values()).index(pred)]
+    img = np.array(img)
+    img = cv2.resize(img, (640, 640))
+    img = img / 255.0
+    img = img.transpose(2, 0, 1)
+    img = torch.from_numpy(img).float().unsqueeze(0).to(device)
+    output = model(img)
+    pred = non_max_suppression(output, conf_thres=0.25, iou_thres=0.45)[0]
+    if pred is not None:
+        pred[:, :4] = scale_coords(img.shape[2:], pred[:, :4], img.shape[2:]).round()
+        class_id = int(pred[0, 5])
+        class_name = model.names[class_id]
+        bboxes = pred[:, :4].cpu().numpy().tolist()
+    else:
+        class_name = 'No detection'
+        bboxes = []
     suggestion = treatment_suggestions.get(class_name, 'No suggestion available.')
-    return jsonify({'class': class_name, 'suggestion': suggestion})
+    return jsonify({'class': class_name, 'suggestion': suggestion, 'bboxes': bboxes})
 
 @app.route('/community_upload', methods=['POST'])
 def community_upload():
