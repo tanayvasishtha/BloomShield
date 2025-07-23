3997c313050631c961119c792912159eb2a9e531
Tanay Vasishtha <tanayvasishtha@gmail.com>
2025-07-23 12:00:49 +0530

    Set epochs to 6 for better accuracy

--- train.py
+++ train.py
@@ -100,7 +100,7 @@ if __name__ == "__main__":
     model.to(device)
 
     start_time = time.time()
-    for epoch in range(4):
+    for epoch in range(6):
         epoch_start = time.time()
         model.train()
         running_loss = 0.0
